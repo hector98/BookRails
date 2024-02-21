083685f032c7b5c9858f669af6ecc4c4f09d1cdf
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2024-02-21 15:53:55 -0600

    vista show para books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,18 @@
 class BooksController < ApplicationController
   def index
-    #@books = Book.all
+    @books = Book.all
+  end
+
+  def show
+    @book = book
+  end
+
+  private
+  def book
+    @book ||= Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
   end
 end
