5034a1997967a3da964706495fb173ffb17661f8
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2024-02-21 17:52:53 -0600

    agregar specs

---
+++ spec/controllers/books_controller_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe BooksController, type: :controller do
+  describe "GET #index" do
+    before do
+      get :index
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the index template" do
+      expect(response).to render_template :index
+    end
+  end
+
+end
